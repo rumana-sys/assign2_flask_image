a0623a72d6514d5083e7b2d7740e9dc2a8d4ce9c
Rumana Khanam <aakhi.zaman@gmail.com>
2025-04-05 12:53:42 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,22 +1,10 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    build_version = os.environ.get('CLOUD_BUILD_VERSION')
-    return f"""
-    <html>
-        <head>
-            <title>Hello Cloud App</title>
-        </head>
-        <body>
-            <h1>Hello Cloud!</h1>
-            <p><strong>CloudBuild VERSION: {build_version}</strong></p>
-        </body>
-    </html>
-    """
+    return "Hello from Khanam ECS Container."
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
